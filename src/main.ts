fd63b4e1f481daa7f16cffced56c57018c458474
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-07 18:37:49 -0300

    chore: install outfit fontface

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import "@fontsource/outfit/500.css"
+import "@fontsource/outfit/700.css"
+
 import './assets/main.css'
 
 createApp(App).mount('#app')
