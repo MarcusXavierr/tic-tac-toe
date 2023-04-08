fff278231aa73cdc991aac1123982a622d81f253
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-08 11:01:49 -0300

    feat: Add BaseButton component with dynamic styling for button color and width

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,84 @@
+<template>
+  <button :class="[color, size]">
+    <slot>BUTTON</slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { BtnColor, BtnWidth } from '@/enums/ButtonTypes'
+
+export default {
+  name: 'BaseButton',
+  props: {
+    buttonColor: {
+      type: Number,
+      required: true
+    },
+    buttonWidth: {
+      type: Number,
+      required: false,
+      default: 0
+    }
+  },
+  computed: {
+    color() {
+      switch (this.buttonColor) {
+        case BtnColor.blue:
+          return 'blue'
+        case BtnColor.yellow:
+          return 'yellow'
+        default:
+          return 'gray'
+      }
+    },
+    size() {
+      if (this.buttonWidth == BtnWidth.lg) {
+        return 'w-full'
+      }
+      return 'normal'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  background: var(--btn-color);
+  color: var(--dark-navy);
+  font-size: 1.25rem;
+  font-weight: 700;
+  text-align: center;
+  border-radius: 1rem;
+  font-family: inherit;
+  padding: 1rem;
+  box-shadow: inset 0px var(--shadow-y-offset, -0.25rem) 0px var(--btn-color-shadow);
+
+  &:hover {
+    background: var(--btn-color-hover);
+  }
+}
+
+.yellow {
+  --btn-color: var(--yellow);
+  --btn-color-hover: var(--yellow-hover);
+  --btn-color-shadow: var(--yellow-shadow);
+}
+
+.blue {
+  --btn-color: var(--blue);
+  --btn-color-hover: var(--blue-hover);
+  --btn-color-shadow: var(--blue-shadow);
+}
+
+.gray {
+  --btn-color: var(--silver);
+  --btn-color-hover: var(--silver-hover);
+  --btn-color-shadow: var(--silver-shadow);
+}
+
+.w-full {
+  width: 100%;
+  padding-bottom: 1.5rem;
+  --shadow-y-offset: -0.5rem;
+}
+</style>
