6b7628fd962d32e0b80a2033b2d4b3b3dc970686
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-08 15:23:09 -0300

    feat(BaseIcon.vue): add BaseIcon component with props for iconType and computed property for iconSource based on iconType value

---
+++ src/components/base/BaseIcon.vue
@@ -0,0 +1,42 @@
+<template>
+  <img :src="iconSource" alt="icon" />
+</template>
+
+<script lang="ts">
+import { IconType } from '@/enums/IconTypes'
+import XIcon from '@/assets/icon-x.svg'
+import XOutlineIcon from '@/assets/icon-x-outline.svg'
+import OIcon from '@/assets/icon-o.svg'
+import OOutlineIcon from '@/assets/icon-o-outline.svg'
+import RestartIcon from '@/assets/icon-restart.svg'
+
+export default {
+  name: 'BaseIcon',
+  props: {
+    iconType: {
+      type: Number,
+      required: true
+    }
+  },
+  mounted() {
+    console.log(this.iconType)
+    console.log(this.iconSource)
+  },
+  computed: {
+    iconSource() {
+      switch (this.iconType) {
+        case IconType.O:
+          return OIcon
+        case IconType.X:
+          return XIcon
+        case IconType.O_outline:
+          return OOutlineIcon
+        case IconType.X_outline:
+          return XOutlineIcon
+        default:
+          return RestartIcon
+      }
+    }
+  }
+}
+</script>
