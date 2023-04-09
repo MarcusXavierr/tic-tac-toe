abb3ac33b15256b76b37a0a77e955691ac4567ae
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-09 16:25:43 -0300

    refactor: store index.ts to remove unnecessary initializations and interfaces

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,13 @@
 import { createStore } from 'vuex'
 
-interface State {
-  foo: string
-}
 export const store = createStore<State>({
   state() {
     return {
-      foo: 'fighters',
       isGameActive: false,
       XPlayer: null,
       OPlayer: null,
-      playHistory: [{
-        position: 1,
-        piece: 1
-      }],
-      gameResults: [{ winner: null }]
+      playHistory: [],
+      gameResults: []
     }
   },
   mutations: {}
