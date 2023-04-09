c65619551aa4f8e81e3e6ad9ac68f5418e4d086c
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-09 16:25:30 -0300

    feat(env.d.ts): add Players enum and State interface for game state management

--- env.d.ts
+++ env.d.ts
@@ -7,3 +7,16 @@ interface moveRecord {
 interface gameResult {
   winner: number | null
 }
+
+enum Players {
+  playerOne = 1,
+  playerTwo = 2
+}
+
+interface State {
+  isGameActive: boolean,
+  XPlayer: number | null,
+  OPlayer: number | null,
+  playHistory: moveRecord[],
+  gameResults: gameResult[]
+}
