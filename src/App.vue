0d99b251a80b090437c1018df76bef399e597b8f
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-09 20:59:59 -0300

    refactor: App.vue to include BaseCell component and update component imports in script

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div class="container">
-    <div v-if="isGameActive">
-      <BaseButton :button-color="btnOptions.colors.yellow" :is-large="true">MAMACO</BaseButton><br>
-      <BaseButton :button-color="btnOptions.colors.blue" />
-      <BaseButton :button-color="btnOptions.colors.gray" :is-large="true"/>
-      <BaseIcon :icon-type="iconOptions.X_outline"/>
-      <BaseButton :button-color="btnOptions.colors.gray">
-        <BaseIcon :icon-type="iconOptions.Restart"/>
-      </BaseButton><br>
-      <BaseButton :button-color="btnOptions.colors.yellow">SECONDARY BUTTON 1</BaseButton>
+    <div v-if="!isGameActive">
+      <BaseCell/>
     </div>
-  <HomePage v-else/>
+    <HomePage v-else />
   </div>
 </template>
 
 <script lang="ts">
 import BaseButton from './components/base/BaseButton.vue'
 import BaseIcon from './components/base/BaseIcon.vue'
+import BaseCell from './components/base/BaseCell.vue'
 import HomePage from './views/Home/index.vue'
 
 import { BtnColor } from './enums/ButtonTypes'
@@ -24,12 +18,17 @@ import { IconType } from './enums/IconTypes'
 
 import { mapState } from 'vuex'
 export default {
-  components: { BaseButton, BaseIcon, HomePage },
+  components: {
+    BaseButton,
+    BaseIcon,
+    HomePage,
+    BaseCell
+  },
   computed: {
     ...mapState(['isGameActive']),
     btnOptions() {
       return {
-        colors: BtnColor,
+        colors: BtnColor
       }
     },
     iconOptions() {
