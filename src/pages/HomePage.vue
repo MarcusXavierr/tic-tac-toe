f5e287e939378c4103fcd03ce26f02b9896a1280
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-08 22:22:06 -0300

    feat(HomePage.vue): add HomePage component with template, script, and style sections

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <img src="../assets/logo.svg" alt="" />
+    <div class="selector">
+      PICK PLAYER 1'S MARK
+      <div class="player-type">
+        <BaseIcon :icon-type="1" />
+        <img src="../assets/icon-o-outline.svg" alt="" />
+      </div>
+      <p class="disclaimer">REMEMBER: X GOES FIRST</p>
+    </div>
+    <div class="buttons">
+      <BaseButton :button-color="btnColors.yellow" :is-large="true">NEW GAME (VS CPU)</BaseButton>
+      <BaseButton :button-color="btnColors.blue" :is-large="true">NEW GAME (VS PLAYER)</BaseButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import BaseIcon from '@/components/base/BaseIcon.vue'
+import BaseButton from '@/components/base/BaseButton.vue'
+import { BtnColor } from '@/enums/ButtonTypes'
+
+export default {
+  name: 'HomePage',
+  components: {
+    BaseIcon,
+    BaseButton
+  },
+  data() {
+    return {
+      btnColors: BtnColor
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+}
+
+.selector {
+  background-color: var(--semi-dark-navy);
+  padding: 1.5rem;
+  font-weight: 700;
+  text-align: center;
+
+  .disclaimer {
+    opacity: 0.5;
+  }
+}
+
+.buttons {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.player-type {
+  img {
+    color: blue;
+  }
+}
+</style>
