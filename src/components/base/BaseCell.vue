407370f1b891d4d66caaaafe3bfe511cc33764aa
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-09 21:03:30 -0300

    feat(BaseCell.vue): add BaseCell component with hover functionality and dynamic icon based on currentPlayerType state variable

---
+++ src/components/base/BaseCell.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="cell" @mouseenter="hover = true" @mouseleave="hover = false">
+    <KeepAlive>
+      <BaseIcon v-if="hover" :icon-type="hoverIcon" />
+    </KeepAlive>
+  </div>
+</template>
+
+<script lang="ts">
+//See optimization to prevent mounting a component multiple times
+import { mapState } from 'vuex'
+import BaseIcon from '../base/BaseIcon.vue'
+import { PlayerTypes } from '@/enums/Players'
+import { IconType } from '@/enums/IconTypes'
+
+export default {
+  name: 'BaseCell',
+  components: { BaseIcon },
+  data() {
+    return {
+      hover: false
+    }
+  },
+  computed: {
+    ...mapState(['currentPlayerType']),
+    hoverIcon() {
+      if (this.currentPlayerType == PlayerTypes.OPlayer) {
+        return IconType.O_outline
+      }
+      if (this.currentPlayerType == PlayerTypes.XPlayer) {
+        return IconType.X_outline
+      }
+      return -1
+    }
+  }
+}
+</script>
+
+<style>
+.cell {
+  background: var(--semi-dark-navy);
+  box-shadow: inset 0 -0.5rem 0 #10212a;
+  padding-bottom: 0.5rem;
+  border-radius: 0.75rem;
+  height: 96px;
+  width: 96px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media (min-width: 767px) {
+  .cell {
+    width: 140px;
+    height: 140px;
+    border-radius: 1rem;
+  }
+}
+</style>
