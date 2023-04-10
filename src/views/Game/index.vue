0dfdabe830deb5b6f06b7625bb3f5cb3ebdaab18
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-09 22:31:45 -0300

    feat: Create GamePage component with NavBar and BaseCell children

---
+++ src/views/Game/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <NavBar />
+    <BaseCell />
+  </div>
+</template>
+
+<script lang="ts">
+import BaseCell from '../../components/base/BaseCell.vue'
+import NavBar from './NavBar.vue'
+
+export default {
+  name: 'GamePage',
+  components: { BaseCell, NavBar }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 1.5rem;
+}
+</style>
