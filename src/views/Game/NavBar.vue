95e43439d107e4dadb5cc8b0fa4ff986d0a209ae
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-09 22:31:55 -0300

    feat: Create NavBar component with logo, turn indicator, and restart button

---
+++ src/views/Game/NavBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="navbar">
+    <img src="../../assets/logo.svg" alt="" />
+    <div class="turn">
+      <img src="../../assets/gray-icons/icon-x.svg" alt="icon representing actual turn" width="16">
+      TURN
+    </div>
+    <BaseButton :button-color="buttonOptions.gray"
+      ><BaseIcon :icon-type="iconOptions.Restart"
+    /></BaseButton>
+  </nav>
+</template>
+
+<script lang="ts">
+import BaseButton from '@/components/base/BaseButton.vue'
+import BaseIcon from '@/components/base/BaseIcon.vue'
+import { BtnColor } from '@/enums/ButtonTypes'
+import { IconType } from '@/enums/IconTypes'
+
+export default {
+  name: 'NavBar',
+  components: {
+    BaseButton,
+    BaseIcon
+  },
+  computed: {
+    buttonOptions() {
+      return BtnColor
+    },
+    iconOptions() {
+      return IconType
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.turn {
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  justify-content: center;
+  background: var(--semi-dark-navy);
+  padding: 0.5rem 1rem 0.75rem 1rem;
+  box-shadow: inset 0px -4px 0px #10212A;
+  border-radius: 0.25rem;
+}
+</style>
