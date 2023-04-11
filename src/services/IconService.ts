5e4c75aa4f95e503461e4362f0bb0673389443e9
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-11 11:55:54 -0300

    feat: Add IconService file with a function to get the icon type based on the player turn

---
+++ src/services/IconService.ts
@@ -0,0 +1,16 @@
+import { IconType } from "@/enums/IconTypes"
+import { PlayerTypes } from "@/enums/Players"
+
+const getIconTypeFromPlayerTurn = (playerTurn: number): number => {
+  if (playerTurn == PlayerTypes.XPlayer) {
+    return IconType.X
+  }
+
+  return IconType.O
+}
+
+
+
+export {
+  getIconTypeFromPlayerTurn
+}
