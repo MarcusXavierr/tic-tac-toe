d88eb535b8638d5ea37c2535db3322d9c841b133
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-11 23:02:28 -0300

    feat(utils): add cleanHistory and rangeWithLeap functions to clean up play history and generate a range of numbers

---
+++ src/services/utils/cleanHistory.ts
@@ -0,0 +1,22 @@
+import type { PlayerTypes } from '../../enums/Players';
+
+export function cleanHistory(piece: PlayerTypes, playHistory: moveRecord[]): moveRecord[] {
+    const sortedHistory = playHistory.sort(sortByPosition);
+    return sortedHistory.filter((item) => item.piece === piece);
+}
+export function rangeWithLeap(start: number, end: number, step: number): number[] {
+    return Array.from({ length: Math.floor((end - start) / step) + 1 }, (_, i) => start + i * step);
+}
+
+function sortByPosition(recordOne: moveRecord, recordTwo: moveRecord): number {
+    if (recordOne.position < recordTwo.position) {
+        return -1;
+    }
+
+    if (recordOne.position > recordTwo.position) {
+        return 1;
+    }
+
+    return 0;
+}
+
