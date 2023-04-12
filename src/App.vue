7a7a73557d208a56da6cf2cc70a5526f78d12d9c
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-12 16:30:52 -0300

    perf: preload images

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,12 @@ import GamePage from './views/Game/index.vue'
 
 import { BtnColor } from './enums/ButtonTypes'
 import { IconType } from './enums/IconTypes'
+import iconO from './assets/icon-o.svg'
+import iconOOutline from './assets/icon-o-outline.svg'
+import iconX from './assets/icon-x.svg'
+import iconXOutline from './assets/icon-x-outline.svg'
+import iconOGray from './assets/gray-icons/icon-o.svg'
+import iconXGrayOutline from './assets/gray-icons/icon-o.svg'
 
 import { mapState } from 'vuex'
 export default {
@@ -30,6 +36,14 @@ export default {
     iconOptions() {
       return IconType
     }
+  },
+  created() {
+    const images = [iconO, iconX, iconOOutline, iconOGray, iconXOutline, iconXGrayOutline]
+
+    images.forEach((path) => {
+      const img = new Image()
+      img.src = path
+    })
   }
 }
 </script>
