2bff89fbf21e23564fe2e47cf0205bc8403ad94f
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-12 12:27:32 -0300

    feat: Add buttons and styling to GameOverModal.vue for a tied game

--- src/components/GameOverModal.vue
+++ src/components/GameOverModal.vue
@@ -1,15 +1,26 @@
 <template>
-  <BaseModal :show="show" @click="$emit('close')"></BaseModal>
+  <BaseModal :show="show" @click="$emit('close')">
+    <div class="container">
+      <h3>ROUND TIED</h3>
+      <div class="btn-group">
+        <BaseButton :button-color="colors.gray">QUIT</BaseButton>
+        <BaseButton :button-color="colors.yellow">NEXT ROUND</BaseButton>
+      </div>
+    </div>
+  </BaseModal>
 </template>
 
 <script lang="ts">
 import BaseModal from './base/BaseModal.vue'
+import BaseButton from './base/BaseButton.vue'
+import { BtnColor } from '@/enums/ButtonTypes'
 
 export default {
   name: 'GameOverModal',
   emits: ['close'],
   components: {
-    BaseModal
+    BaseModal,
+    BaseButton
   },
   props: {
     show: Boolean,
@@ -17,6 +28,29 @@ export default {
       type: Number,
       required: true
     }
+  },
+  computed: {
+    colors() {
+      return BtnColor
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+  height: 16.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1.5rem;
+  h3 {
+    font-size: 2rem;
+    letter-spacing: 1.5px;
+  }
+}
+.btn-group {
+  display: flex;
+  gap: 1rem;
+}
+</style>
