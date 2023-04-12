47b21b23eb9be5c8eb3a3e51628568413a663d09
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-12 13:18:32 -0300

    feat(GameOverModal.vue): add support for displaying winner and round result messages

--- src/components/GameOverModal.vue
+++ src/components/GameOverModal.vue
@@ -1,7 +1,26 @@
 <template>
   <BaseModal :show="show" @click="$emit('close')">
     <div class="container">
-      <h3>ROUND TIED</h3>
+      <div class="messages">
+        <!-- TIE -->
+        <h3 v-if="winner == -1">ROUND TIED</h3>
+        <!-- O player -->
+        <div class="player" v-else-if="winner == playerTypes.OPlayer">
+          <p>PLAYER {{ playerWinner }} WINS!</p>
+          <h3 class="flex-header">
+            <BaseIcon :icon-type="icons.O" class="icon" />
+            <span class="yellow">TAKES THE ROUND</span>
+          </h3>
+        </div>
+        <!-- X player -->
+        <div class="player" v-else-if="winner == playerTypes.XPlayer">
+          <p>PLAYER {{ playerWinner }} WINS!</p>
+          <h3 class="flex-header">
+            <BaseIcon :icon-type="icons.X" class="icon" />
+            <span class="blue">TAKES THE ROUND</span>
+          </h3>
+        </div>
+      </div>
       <div class="btn-group">
         <BaseButton :button-color="colors.gray">QUIT</BaseButton>
         <BaseButton :button-color="colors.yellow">NEXT ROUND</BaseButton>
@@ -13,25 +32,40 @@
 <script lang="ts">
 import BaseModal from './base/BaseModal.vue'
 import BaseButton from './base/BaseButton.vue'
+import BaseIcon from './base/BaseIcon.vue'
 import { BtnColor } from '@/enums/ButtonTypes'
+import { PlayerTypes } from '@/enums/Players'
+import { IconType } from '@/enums/IconTypes'
 
 export default {
   name: 'GameOverModal',
   emits: ['close'],
   components: {
     BaseModal,
-    BaseButton
+    BaseButton,
+    BaseIcon
   },
   props: {
     show: Boolean,
     winner: {
       type: Number,
       required: true
+    },
+    playerWinner: {
+      type: Number,
+      required: false,
+      default: undefined
     }
   },
   computed: {
     colors() {
       return BtnColor
+    },
+    playerTypes() {
+      return PlayerTypes
+    },
+    icons() {
+      return IconType
     }
   }
 }
@@ -41,6 +75,8 @@ export default {
 .container {
   height: 16.5rem;
   display: flex;
+  text-align: center;
+  align-items: center;
   flex-direction: column;
   justify-content: center;
   gap: 1.5rem;
@@ -52,5 +88,33 @@ export default {
 .btn-group {
   display: flex;
   gap: 1rem;
+}
+.player {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  p {
+    font-weight: 700;
+  }
+}
+
+.flex-header {
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+
+  .icon {
+    width: 2rem;
+    height: 2rem;
+  }
+
+  .yellow {
+    color: var(--yellow);
+  }
+
+  .blue {
+    color: var(--blue);
+  }
 }
 </style>
