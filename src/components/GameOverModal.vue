ff371c159caeb56a5b4f4f03d0b021a8f454cdbf
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-12 11:40:52 -0300

    feat(GameOverModal.vue): add GameOverModal component with BaseModal and winner prop

---
+++ src/components/GameOverModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <BaseModal :show="show" @click="$emit('close')"></BaseModal>
+</template>
+
+<script lang="ts">
+import BaseModal from './base/BaseModal.vue'
+
+export default {
+  name: 'GameOverModal',
+  emits: ['close'],
+  components: {
+    BaseModal
+  },
+  props: {
+    show: Boolean,
+    winner: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
