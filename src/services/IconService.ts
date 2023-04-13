a69f724005fe0a6c872dbb686eb02c8d7ed4f864
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-13 13:57:16 -0300

    refactor: changing function signature to more specific types

--- src/services/IconService.ts
+++ src/services/IconService.ts
@@ -1,7 +1,7 @@
 import { IconType } from "@/enums/IconTypes"
 import { PlayerTypes } from "@/enums/Players"
 
-const getIconTypeFromPlayerTurn = (playerTurn: number): number => {
+const getIconTypeFromPlayerTurn = (playerTurn: PlayerTypes): IconType => {
   if (playerTurn == PlayerTypes.XPlayer) {
     return IconType.X
   }
