3a46b1b138660faade647f57ad34f910098a03a2
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-14 18:56:34 -0300

    feat: dinamically change cell color and icon based on winner path

--- src/components/base/BaseCell.vue
+++ src/components/base/BaseCell.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="cell" @mouseenter="hover = true" @mouseleave="hover = false">
+  <div class="cell" :class="color" @mouseenter="hover = true" @mouseleave="hover = false">
     <KeepAlive>
-      <BaseIcon v-if="selectedIcon != null" :icon-type="selectedIcon"/>
+      <BaseIcon v-if="selectedIcon != null" :icon-type="icon" />
       <BaseIcon v-else-if="hover && !isWaitingToPlay" :icon-type="hoverIcon" />
     </KeepAlive>
   </div>
@@ -27,7 +27,15 @@ export default {
       type: Number as PropType<IconType | null>,
       required: false,
       default: null
+    },
+    belongsToWinnerPath: {
+      type: Boolean,
+      required: false,
+      default: false
     }
+  },
+  mounted() {
+    console.log(this.belongsToWinnerPath, this.selectedIcon)
   },
   computed: {
     ...mapState(['currentPlayerType', 'isWaitingToPlay']),
@@ -39,15 +47,40 @@ export default {
         return IconType.X_outline
       }
       return -1
+    },
+    icon(): IconType {
+      if (!this.belongsToWinnerPath) {
+        return this.selectedIcon as IconType
+      }
+
+      if (this.selectedIcon == IconType.X) {
+        return IconType.X_dark
+      }
+
+      return IconType.O_dark
+    },
+    color() {
+      if (!this.belongsToWinnerPath) {
+        return ''
+      }
+
+      if (this.selectedIcon == IconType.X) {
+        return 'blue'
+      }
+
+      return 'yellow'
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .cell {
-  background: var(--semi-dark-navy);
-  box-shadow: inset 0 -0.5rem 0 #10212a;
+  --bg-cell: var(--semi-dark-navy);
+  --border-color: #10212a;
+
+  background: var(--bg-cell);
+  box-shadow: inset 0 -0.5rem 0 var(--border-color);
   padding-bottom: 0.5rem;
   border-radius: 0.75rem;
   min-height: 96px;
@@ -61,6 +94,15 @@ export default {
   -webkit-tap-highlight-color: transparent;
 }
 
+.yellow {
+  --bg-cell: var(--yellow);
+  --border-color: var(--yellow-shadow);
+}
+
+.blue {
+  --bg-cell: var(--blue);
+  --border-color: var(--blue-shadow);
+}
 @media (min-width: 767px) {
   .cell {
     border-radius: 1rem;
