25042db3cedc9518a67bb36f2a8c48e3dcc613f7
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-14 19:00:48 -0300

    chore: Export the sortByPosition function in cleanHistory.ts to be used elsewhere

--- src/services/utils/cleanHistory.ts
+++ src/services/utils/cleanHistory.ts
@@ -8,7 +8,7 @@ export function rangeWithLeap(start: number, end: number, step: number): number[
     return Array.from({ length: Math.floor((end - start) / step) + 1 }, (_, i) => start + i * step);
 }
 
-function sortByPosition(recordOne: moveRecord, recordTwo: moveRecord): number {
+export function sortByPosition(recordOne: moveRecord, recordTwo: moveRecord): number {
     if (recordOne.position < recordTwo.position) {
         return -1;
     }
