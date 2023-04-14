8afc5851bece9f5613ca9ffd9e79c8207ea7c80a
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-14 18:59:53 -0300

    test: Add findWinnerPath and markWinnerPath to GameService in order to find and mark the winner path in Tic Tac Toe game

--- src/tests/unit/services/GameService.test.ts
+++ src/tests/unit/services/GameService.test.ts
@@ -1,6 +1,6 @@
 import { IconType } from '../../../enums/IconTypes'
 import { PlayerTypes } from '../../../enums/Players'
-import { determineWinner } from '../../../services/GameService'
+import { determineWinner, findWinnerPath, markWinnerPath } from '../../../services/GameService'
 import { describe, it, expect } from 'vitest'
 
 describe('Find out the winner on horizontal', () => {
@@ -36,7 +36,7 @@ describe('Find out the winner on horizontal', () => {
       ],
       want: null,
       name: 'null'
-    },
+    }
   ]
 
   cases.forEach((tt) => {
@@ -77,7 +77,7 @@ describe('Find out the winner on vertical', () => {
       ],
       want: null,
       name: 'null'
-    },
+    }
   ]
 
   cases.forEach((tt) => {
@@ -94,7 +94,7 @@ describe('Find out the winner on diagonal', () => {
       history: [
         { piece: IconType.X, position: 1 },
         { piece: IconType.X, position: 9 },
-        { piece: IconType.X, position: 5 },
+        { piece: IconType.X, position: 5 }
       ],
       want: PlayerTypes.XPlayer,
       name: 'X'
@@ -103,7 +103,7 @@ describe('Find out the winner on diagonal', () => {
       history: [
         { piece: IconType.O, position: 7 },
         { piece: IconType.O, position: 3 },
-        { piece: IconType.O, position: 5 },
+        { piece: IconType.O, position: 5 }
       ],
       want: PlayerTypes.OPlayer,
       name: 'O'
@@ -113,11 +113,11 @@ describe('Find out the winner on diagonal', () => {
         { piece: IconType.X, position: 1 },
         { piece: IconType.X, position: 9 },
         { piece: IconType.O, position: 7 },
-        { piece: IconType.O, position: 3 },
+        { piece: IconType.O, position: 3 }
       ],
       want: null,
       name: 'null'
-    },
+    }
   ]
 
   cases.forEach((tt) => {
@@ -128,4 +128,79 @@ describe('Find out the winner on diagonal', () => {
   })
 })
 
+describe('find the winner pattern', () => {
+  const cases = [
+    {
+      history: [
+        { piece: IconType.X, position: 1 },
+        { piece: IconType.X, position: 2 },
+        { piece: IconType.X, position: 4 },
+        { piece: IconType.O, position: 3 },
+        { piece: IconType.X, position: 9 },
+        { piece: IconType.X, position: 5 }
+      ],
+      piece: IconType.X,
+      want: [1, 5, 9],
+      name: 'diagonal'
+    },
+    {
+      history: [
+        { piece: IconType.O, position: 1 },
+        { piece: IconType.O, position: 3 },
+        { piece: IconType.O, position: 4 },
+        { piece: IconType.O, position: 7 },
+        { piece: IconType.O, position: 8 }
+      ],
+      piece: IconType.O,
+      want: [1, 4, 7],
+      name: 'vertical'
+    },
+    {
+      history: [
+        { piece: IconType.X, position: 1 },
+        { piece: IconType.X, position: 7 },
+        { piece: IconType.O, position: 6 },
+        { piece: IconType.O, position: 4 },
+        { piece: IconType.O, position: 5 },
+        { piece: IconType.O, position: 8 }
+      ],
+      piece: IconType.O,
+      want: [4, 5, 6],
+      name: 'horizontal'
+    }
+  ]
+
+  cases.forEach((tt) => {
+    it(`should return ${tt.name} as winner`, () => {
+      const result = findWinnerPath(tt.piece, tt.history)
+      expect(result).toStrictEqual(tt.want)
+    })
+  })
+})
+
+describe('map winner path to history', () => {
+  const history = [
+    { piece: IconType.O, position: 1 },
+    { piece: IconType.O, position: 2 },
+    { piece: IconType.X, position: 4 },
+    { piece: IconType.O, position: 3 },
+    { piece: IconType.X, position: 9 },
+    { piece: IconType.X, position: 5 }
+  ]
+  const path = [1, 2, 3]
+
+  const result = markWinnerPath(history, path)
+  const want = [
+    { piece: IconType.O, position: 1, belongsToWinnerPath: true },
+    { piece: IconType.O, position: 2, belongsToWinnerPath: true },
+    { piece: IconType.X, position: 4 , belongsToWinnerPath: false },
+    { piece: IconType.O, position: 3, belongsToWinnerPath: true },
+    { piece: IconType.X, position: 9 , belongsToWinnerPath: false },
+    { piece: IconType.X, position: 5 , belongsToWinnerPath: false }
+  ]
+
+  it('should fill all fields', () => {
+    expect(result).toStrictEqual(want)
+  })
+})
 export {}
