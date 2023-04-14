5c9a9160bad3a0d6383dec43f011aae7764135e4
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-14 19:04:39 -0300

    feat: change return type of helper functions to use it to find the winner path

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -1,6 +1,6 @@
 import { IconType } from '../enums/IconTypes'
 import { PlayerTypes } from '../enums/Players'
-import { cleanHistory, rangeWithLeap } from './utils/cleanHistory'
+import { cleanHistory, rangeWithLeap, sortByPosition } from './utils/cleanHistory'
 
 export function determineWinner(playHistory: moveRecord[]): PlayerTypes | null {
   if (playHistory.length === 0) {
@@ -22,38 +22,47 @@ function findRow(piece: IconType, playHistory: moveRecord[]): boolean {
   const history = cleanHistory(piece, playHistory)
 
   return (
-    findHorizontalPattern(history) || findVerticalPattern(history) || findDiagonalPatter(history)
+    findHorizontalPattern(history).length > 0 ||
+    findVerticalPattern(history).length > 0 ||
+    findDiagonalPatter(history).length > 0
   )
 }
 
-function findHorizontalPattern(history: moveRecord[]): boolean {
+function findHorizontalPattern(history: moveRecord[]): number[] {
   const cases = rangeWithLeap(1, 9, 3)
 
-  return cases.reduce((acc, i) => {
-    if (acc) return acc
+  return cases.reduce<number[]>((acc, i) => {
+    if (acc.length > 0) return acc
 
     const fst = history.some((move) => move.position === i)
     const snd = history.some((move) => move.position === i + 1)
     const third = history.some((move) => move.position === i + 2)
 
-    return fst && snd && third
-  }, false)
+    if (fst && snd && third) {
+      return acc.concat([i, i + 1, i + 2])
+    }
+    return []
+  }, [])
 }
 
-function findVerticalPattern(history: moveRecord[]): boolean {
+function findVerticalPattern(history: moveRecord[]): number[] {
   const cases = rangeWithLeap(1, 3, 1)
 
-  return cases.reduce((acc, i) => {
-    if (acc) return acc
+  return cases.reduce<number[]>((acc, i) => {
+    if (acc.length > 0) return acc
+
     const fst = history.some((move) => move.position === i)
     const snd = history.some((move) => move.position === i + 3)
     const third = history.some((move) => move.position === i + 6)
 
-    return fst && snd && third
-  }, false)
+    if (fst && snd && third) {
+      return acc.concat([i, i + 3, i + 6])
+    }
+    return []
+  }, [])
 }
 
-function findDiagonalPatter(history: moveRecord[]): boolean {
+function findDiagonalPatter(history: moveRecord[]): number[] {
   const mainAxis =
     history.some((move) => move.position === 1) &&
     history.some((move) => move.position === 5) &&
@@ -64,5 +73,12 @@ function findDiagonalPatter(history: moveRecord[]): boolean {
     history.some((move) => move.position === 5) &&
     history.some((move) => move.position === 7)
 
-  return mainAxis || secondaryAxis
+  if (mainAxis) {
+    return [1, 5, 9]
+  }
+
+  if (secondaryAxis) {
+    return [3, 5, 7]
+  }
+  return []
 }
