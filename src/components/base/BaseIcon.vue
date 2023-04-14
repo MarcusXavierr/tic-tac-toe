c999c98bd7233ef8e5ecda2e7e32833d24dd9aae
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-14 18:56:55 -0300

    feat: add dark icon options on BaseIcon

--- src/components/base/BaseIcon.vue
+++ src/components/base/BaseIcon.vue
@@ -9,6 +9,8 @@ import XOutlineIcon from '@/assets/icon-x-outline.svg'
 import OIcon from '@/assets/icon-o.svg'
 import OOutlineIcon from '@/assets/icon-o-outline.svg'
 import RestartIcon from '@/assets/icon-restart.svg'
+import ODarkIcon from '@/assets/icon-o-dark.svg'
+import XDarkIcon from '@/assets/icon-x-dark.svg'
 
 export default {
   name: 'BaseIcon',
@@ -29,6 +31,10 @@ export default {
           return OOutlineIcon
         case IconType.X_outline:
           return XOutlineIcon
+        case IconType.O_dark:
+          return ODarkIcon
+        case IconType.X_dark:
+          return XDarkIcon
         default:
           return RestartIcon
       }
