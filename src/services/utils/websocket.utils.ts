5d7c37e2668fab42426faeb3c38542987df3c625
Marcus Xavier <marcusxavierr123@gmail.com>
2024-05-02 21:40:47 -0300

    fix: fix validation bug on zod

--- src/services/utils/websocket.utils.ts
+++ src/services/utils/websocket.utils.ts
@@ -10,10 +10,10 @@ export function parseMovementMessage(message: Message): MoveRecord & {userId: st
       userId: z.string(),
       position: z.number(),
       piece: z.nativeEnum(PlayerTypes),
-      belongsToWinnerPath: z.boolean().optional(),
+      belongsToWinnerPath: z.boolean().optional().nullable(),
     });
 
-    return parser.parse(body);
+    return parser.parse(body) as MoveRecord & {userId: string};
   } catch (e) {
     console.error(e);
     throw new Error("Failed to parse websocket message!");
