04d8feb51976cbb8028c3b2f5094068ef5ec6409
Marcus Xavier <marcusxavierr123@gmail.com>
2024-05-02 21:35:12 -0300

    feat: add zod to validate message received from the server and other refactoring

--- src/services/Websocket.service.ts
+++ src/services/Websocket.service.ts
@@ -1,29 +1,37 @@
 import { Client, over } from "webstomp-client";
 import { store } from "@/store";
-import type { Room } from "./OnlineGame.service";
-import { Players, PlayerTypes } from "@/enums/Players";
+import type { Room } from "./MultiplayerGame.service";
+import { PlayerTypes } from "@/enums/Players";
+import { getPlayersType } from "./utils/player";
+import { parseMovementMessage } from "./utils/websocket.utils";
 
 export class WebsocketService {
   private socket: WebSocket;
   private client: Client;
   // TODO: Colocar variaveis de ambiente no .env
-  private readonly brokerUrl = 'ws://localhost:8080/ws/join';
-  private readonly messageBrokerBase = '/message-broker';
-  private readonly messageListenerBase = '/app'
+  private readonly stompEndpoint = 'ws://localhost:8080/ws/join';
+  private readonly broker = '/message-broker';
+  private readonly listener = '/app'
+  // TODO: mount this paths on constructor to ensure the this part is not null
+  private readonly gameRoomPath = `${this.broker}/rooms`;
+  private readonly roomWaitingPath = `${this.broker}/room-joined`;
+  private readonly roomJoinedPath = `${this.listener}/room-joined`;
   private readonly userId: string;
 
   constructor(userId: string) {
     // BUG: Sometimes the client can't connect to the websocket server. Need investigate why
     // and don't let the user start a multiplayer game until they have a websocket connection running
-    this.socket = new WebSocket(this.brokerUrl);
+    this.socket = new WebSocket(this.stompEndpoint);
     this.client = over(this.socket);
-    store.state.websocketClient = this.client;
+
+    store.commit('setWebsocketClient', this.client);
     store.state.userId = userId;
     this.userId = userId;
+
+    this.gameRoomPath = `${this.broker}/rooms`;
   }
 
   public handleCreatorConnection(room: Room) {
-    store.state.room = room;
     this.client.connect({}, this.getCreateRoomCallback(room), (error) => {
       store.commit('setRoomWaitingState', false)
       console.error(error);
@@ -31,7 +39,6 @@ export class WebsocketService {
   }
 
   public handleJoinerConnection(room: Room) {
-    store.state.room = room;
     this.client.connect({}, this.getJoinRoomCallback(room), (error) => {
       store.commit('setRoomWaitingState', false)
       console.error(error);
@@ -41,30 +48,21 @@ export class WebsocketService {
   public getCreateRoomCallback(room: Room): (frame?: any) => any {
     const _this = this
     return () => {
-      // Subscribe to the room
-      // TODO: refactor to put this rooms in a constant too
-      _this.client.subscribe(`${_this.messageBrokerBase}/rooms/${room.roomId}`, (message) => {
-        // If the current player is the creator of the room, skip the message handling because was the one who sent it
-        const body = JSON.parse(message.body) as MoveRecord & { userId: string };
-        if (body.userId === _this.userId) {
-          return
+      _this.client.subscribe(`${_this.gameRoomPath}/${room.roomId}`, (message) => {
+        const body = parseMovementMessage(message);
+        if (body.userId !== _this.userId) {
+          store.commit('addPlayToHistory', body);
         }
-
-        store.commit('addPlayToHistory', body);
       })
 
-      // Waits for the other player to join
-      // and start the game
-      // TODO: refactor to put this room-joined in a constant too
-      _this.client.subscribe(`${_this.messageBrokerBase}/room-joined/${room.roomId}`, () => {
+      // Wait for the other player to join the room
+      _this.client.subscribe(`${_this.roomWaitingPath}/${room.roomId}`, () => {
         store.commit('activateOnlineGame', getPlayersType(room))
 
-        // Waits for the first move from the other player if the current player's piece is O
         if (room.creatorPiece === PlayerTypes.OPlayer) {
           store.commit('makePlayersWait')
         }
 
-        // Now I can finally remove the waiting and let the user play
         store.commit('setRoomWaitingState', false)
       })
     }
@@ -73,21 +71,14 @@ export class WebsocketService {
   public getJoinRoomCallback(room: Room): (frame?: any) => any {
     const _this = this
     return () => {
-      // TODO: refactor to put this rooms in a constant too
-      _this.client.subscribe(`${_this.messageBrokerBase}/rooms/${room.roomId}`, (message) => {
-        // If the current player is the creator of the room, skip the message handling because was the one who sent it
-        const body = JSON.parse(message.body) as MoveRecord & { userId: string };
-        if (body.userId === _this.userId) {
-          return
+      _this.client.subscribe(`${_this.gameRoomPath}/${room.roomId}`, (message) => {
+        const body = parseMovementMessage(message);
+        if (body.userId !== _this.userId) {
+          store.commit('addPlayToHistory', body);
         }
-
-        console.log('recebi a jogada: e vou aplicar ela', body);
-        store.commit('addPlayToHistory', body);
       })
 
-      // TODO: refactor to put this room-joined in a constant too
-      _this.client.send(`${_this.messageListenerBase}/room-joined/${room.roomId}`, JSON.stringify({ roomId: room.roomId }));
-
+      _this.client.send(`${_this.roomJoinedPath}/${room.roomId}`, JSON.stringify({ roomId: room.roomId }));
       const players = getPlayersType(room)
       store.commit('activateOnlineGame', players)
 
@@ -95,18 +86,5 @@ export class WebsocketService {
         store.commit('makePlayersWait')
       }
     }
-  }
-}
-
-function getPlayersType(room: Room): { OPlayer: Players; XPlayer: Players } {
-  if (room.creatorPiece === PlayerTypes.OPlayer) {
-    return {
-      OPlayer: Players.playerOne,
-      XPlayer: Players.playerTwo,
-    }
-  }
-  return {
-    XPlayer: Players.playerOne,
-    OPlayer: Players.playerTwo,
   }
 }
--- src/services/utils/player.ts
+++ src/services/utils/player.ts
@@ -1,4 +1,6 @@
 import { IconType } from "@/enums/IconTypes";
+import { Players, PlayerTypes } from "@/enums/Players";
+import type { Room } from "../MultiplayerGame.service";
 
 export function swapIconType(icon: IconType) {
   if (icon == IconType.O) {
@@ -6,4 +8,17 @@ export function swapIconType(icon: IconType) {
   }
 
   return IconType.O
+}
+
+export function getPlayersType(room: Room): { OPlayer: Players; XPlayer: Players } {
+  if (room.creatorPiece === PlayerTypes.OPlayer) {
+    return {
+      OPlayer: Players.playerOne,
+      XPlayer: Players.playerTwo,
+    }
+  }
+  return {
+    XPlayer: Players.playerOne,
+    OPlayer: Players.playerTwo,
+  }
 }
