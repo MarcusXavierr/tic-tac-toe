28440e4ddb74c665a79795c7b93186d26f16dfdd
Marcus Xavier <marcusxavierr123@gmail.com>
2026-03-28 11:24:31 -0300

    Revert "feat: creating function to delete room when leaving game"
    This reverts commit ca776d35b2965160f91ecc053b5613d7db689b78.

--- src/services/MultiplayerGame.service.ts
+++ src/services/MultiplayerGame.service.ts
@@ -1,59 +1,39 @@
 import type { PlayerTypes } from "@/enums/Players";
 import { store } from "@/store";
 import { v4 as uuidv4 } from 'uuid';
-import type { Client } from "webstomp-client";
 import { WebsocketService } from "./Websocket.service";
 
 export class MultiplayerGameService {
   public readonly userId: string;
   private readonly ws: WebsocketService;
-  private readonly httpRoomsEndpoint: string = import.meta.env.VITE_HTTP_ROOMS_ENDPOINT;
 
   public constructor() {
     this.userId = uuidv4();
     this.ws = new WebsocketService(this.userId);
   }
 
-  async createRoom(playerPiece: PlayerTypes) {
+  createRoom(playerPiece: PlayerTypes) {
     store.commit('setRoomWaitingState', true);
-    this.detachFromOlderConnections()
-    try {
-      const room = await this.generateRoom(playerPiece)
-      store.commit('setRoom', room);
-      this.ws.handleCreatorConnection(room);
-
-    } catch (error) {
-      console.error(error);
-      store.commit('setRoomWaitingState', false);
-    }
+    this.generateRoom(playerPiece)
+      .then(room => {
+        store.commit('setRoom', room);
+        this.ws.handleCreatorConnection(room);
+      })
+      .catch(err => {
+        console.error(err);
+        store.commit('setRoomWaitingState', false);
+      });
   }
 
-  async joinRoom(roomId: string) {
-    this.detachFromOlderConnections()
-    try {
-      const room = await this.getRoom(roomId)
-      store.commit('setRoom', room);
-      this.ws.handleJoinerConnection(room);
-
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  private detachFromOlderConnections() {
-    // unsubscribe from the room's websocket
-    const client = store.state.websocketClient as Client | undefined;
-    const room = store.state.room as Room | undefined;
-    if (client && room) {
-      client.unsubscribe(`${this.ws.gameRoomPath}/${room.roomId}`);
-
-      if (room.creatorId === this.userId) {
-        client.unsubscribe(`${this.ws.roomWaitingPath}/${room.roomId}`);
-      }
-    }
-
-    // Delete the room from the server
-    fetch(`${this.httpRoomsEndpoint}/${room?.roomId}`, {method: 'DELETE'}).catch(console.error)
+  joinRoom(roomId: string) {
+    this.getRoom(roomId)
+      .then(room => {
+        store.commit('setRoom', room);
+        this.ws.handleJoinerConnection(room);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
   private async generateRoom(playerPiece: PlayerTypes) {
@@ -63,7 +43,8 @@ export class MultiplayerGameService {
       creatorPiece: playerPiece.valueOf()
     }
 
-    const response = await fetch(this.httpRoomsEndpoint, {
+    // TODO: colocar isso num .env
+    const response = await fetch('http://localhost:8080/api/rooms', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -81,7 +62,8 @@ export class MultiplayerGameService {
 
 
   private async getRoom(roomId: string) {
-    const response = await fetch(`${this.httpRoomsEndpoint}/${roomId}`)
+    // TODO: colocar isso num .env
+    const response = await fetch(`http://localhost:8080/api/rooms/${roomId}`)
     if (!response.ok) {
       throw new Error('Failed to get room');
     }
